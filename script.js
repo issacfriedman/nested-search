3dbcee1ffa680ac1d1686d83f34f01fdde218a14
Issac Friedman <issacfriedman613@gmail.com>
2021-06-13 08:36:37 -0400

    init

---
+++ script.js
@@ -0,0 +1,68 @@
+const test = 78;
+//this is a test obj
+
+const apiData = [23, 89, 234, 23, 54, 23, "issac"];
+
+const nestedStringSearch = function (data, searchTerm) {
+  const res = [];
+  const search = function (data) {
+    if (typeof data !== "object") {
+      if (String(data).includes(searchTerm)) res.push(data);
+      return;
+    }
+    for (let prop in data) {
+      if (typeof data[prop] !== "object") {
+        if (String(data[prop]).includes(searchTerm)) res.push(data[prop]);
+      }
+      if (Array.isArray(data[prop])) {
+        data[prop].forEach((el) => {
+          if (typeof el !== "object") {
+            if (String(el).includes(searchTerm)) res.push(el);
+          } else {
+            search(el);
+          }
+        });
+      }
+      if (typeof data[prop] === "object") {
+        search(data[prop]);
+      }
+    }
+  };
+
+  search(data);
+  const set = new Set(res);
+  return [...set];
+};
+
+const nestedSearch = function (data, callbackfn) {
+  if (typeof callbackfn !== "function")
+    throw "callback peramrter must be a function";
+  const res = [];
+  const search = function (data) {
+    if (typeof data !== "object") {
+      if (callbackfn(data)) res.push(data);
+      return;
+    }
+    for (let prop in data) {
+      if (typeof data[prop] !== "object") {
+        if (callbackfn(data[prop])) res.push(data[prop]);
+      }
+      if (Array.isArray(data[prop])) {
+        data[prop].forEach((el) => {
+          if (typeof el !== "object") {
+            if (callbackfn(el)) res.push(el);
+          } else {
+            search(el);
+          }
+        });
+      }
+      if (typeof data[prop] === "object") {
+        search(data[prop]);
+      }
+    }
+  };
+
+  search(data);
+  const set = new Set(res);
+  return [...set];
+};
